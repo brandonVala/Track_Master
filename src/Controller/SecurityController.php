b466b2a61a8a18782ffbacb35f79a461de13fd29
Brandon <brandonvaladez346@gmail.com>
2023-07-08 10:43:22 -0600

    ACTUAL

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -28,11 +28,24 @@ class SecurityController extends AbstractController
     }
 
     /**
-     * @Route("/logout", name="app_logout")
-     */
-    public function logout(): void
-    {
-        // Este método será interceptado por la clave "logout" en el firewall de tu archivo security.yaml.
-        // No es necesario agregar lógica adicional aquí.
-    }
+ * @Route("/logout", name="app_logout")
+ */
+public function logout(UrlGeneratorInterface $urlGenerator): Response
+{
+    // Este método será interceptado por la clave "logout" en el firewall de tu archivo security.yaml.
+    // No es necesario agregar lógica adicional aquí.
+
+    // Crear una nueva respuesta sin contenido
+    $response = new RedirectResponse($urlGenerator->generate('app_login'));
+
+    // Agregar un parámetro de consulta único para evitar la carga desde la caché
+    $response->headers->set('Cache-Control', 'no-store, must-revalidate');
+    $response->headers->set('Pragma', 'no-cache');
+    $response->headers->set('Expires', '0');
+    $response->headers->set('Pragma', 'no-cache');
+    $response->headers->set('Cache-Control', 'no-cache, must-revalidate');
+
+    return $response;
+}
+
 }
